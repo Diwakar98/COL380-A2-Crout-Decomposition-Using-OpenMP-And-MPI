a17da0a1f634039cedc55c293388e30b93485310
Diwakar <diwakarprajapati18222@gmail.com>
2021-04-28 20:57:52 +0530

    done 1, 2, 3

--- main.c
+++ main.c
@@ -4,56 +4,12 @@
 #include <string.h>
 
 int number_of_threads;
+double **A;
+double **L;
+double **U;
 
-/*
-void crout0(double **A, double **L, double **U, int n) {
-    int i, j, k;
-    double sum = 0;
-    for (i = 0; i < n; i++) {
-        U[i][i] = 1;
-    }
-    for (j = 0; j < n; j++) {
-        printf("j : %d\n",j);
-        sum = 0;
-        // printf("\ti : %d\n",j);
-        printf("------------------------------\n");
-        for (k = 0; k < j; k++) {
-            sum = sum + L[j][k] * U[k][j];
-            // printf("\t\tREAD: \tL[%d,%d]\n",j,k);
-            // printf("\t\tREAD: \tU[%d,%d]\n",k,j);
-        }
-        L[j][j] = A[j][j] - sum;
-        for (i = j+1; i < n; i++) {
-            sum = 0;
-            for (k = 0; k < j; k++) {
-                sum = sum + L[i][k] * U[k][j];
-                printf("\t\tREAD: \tL[%d,%d]\n",i,k);
-                printf("\t\tREAD: \tU[%d,%d]\n",k,j);
-            }
-            L[i][j] = A[i][j] - sum;
-            printf("\t\tWRITE: \tL[%d,%d]\n",i,j);
-        }
-        printf("--------\n");
-        for (i = j; i < n; i++) {
-            sum = 0;
-            // printf("\ti : %d\n",i);
-            for(k = 0; k < j; k++) {
-                sum = sum + L[j][k] * U[k][i];
-                printf("\t\tREAD: \tL[%d,%d]\n",j,k);
-                printf("\t\tREAD: \tU[%d,%d]\n",k,i);
-            }
-            if (L[j][j] == 0) {				
-                exit(0);
-            }
-            U[j][i] = (A[j][i] - sum) / L[j][j];
-            printf("\t\tREAD: \tL[%d,%d]\n",j,j);
-            printf("\t\tWRITE: \tU[%d,%d]\n",j,i);
-        }
-    }
-}	
-*/
 
-void crout0(double **A, double **L, double **U, int n) {
+void crout0(double **L, double **U, int n) {
 	int i, j, k;
 	double sum = 0;
 	for (i = 0; i < n; i++) {
@@ -79,78 +35,42 @@ void crout0(double **A, double **L, double **U, int n) {
 		}
 	}
 }	
-/*
-void crout1(double **A, double **L, double **U, int n, int number_of_threads) {
+
+void crout1(double **L, double **U, int n, int number_of_threads) {
     int i, j, k;
     double sum = 0;
-    #pragma parallel for num_threads(number_of_threads)
+    #pragma omp parallel for num_threads(number_of_threads) private(i)
     for (i = 0; i < n; i++) {
         U[i][i] = 1;
     }
     for (j = 0; j < n; j++) {
-        #pragma parallel for num_threads(number_of_threads)
+        #pragma omp parallel for num_threads(number_of_threads) private(i,k,sum)
         for (i = j; i < n; i++) {
-            printf("X\n");
             sum = 0;
             for (k = 0; k < j; k++) {
                 sum = sum + L[i][k] * U[k][j];	
             }
             L[i][j] = A[i][j] - sum;
         }
-        #pragma parallel for num_threads(number_of_threads)
-        for (i = j; i < n; i++) {
-            printf("Y\n");
-            sum = 0;
-            for(k = 0; k < j; k++) {
-                sum = sum + L[j][k] * U[k][i];
-            }
-            if (L[j][j] == 0) {				
-                exit(0);
-            }
-            U[j][i] = (A[j][i] - sum) / L[j][j];
-        }
-    }
-}	*/
 
-void crout1(double **A, double **L, double **U, int n, int number_of_threads) {
-    int i, j, k;
-    double sum = 0;
-    #pragma parallel for num_threads(number_of_threads)
-    for (i = 0; i < n; i++) {
-        U[i][i] = 1;
-    }
-    for (j = 0; j < n; j++) {
-        sum =0 ;
-        for (k = 0; k < j; k++) {
-            sum = sum + L[j][k] * U[k][j];	
-        }
-        L[j][j] = A[j][j] - sum;
-
-        #pragma parallel for num_threads(number_of_threads)
-        for (i = j; i < n; i++) {
-            if(i>j){
-                sum = 0;
-                for (k = 0; k < j; k++) {
-                    sum = sum + L[i][k] * U[k][j];	
-                }
-                L[i][j] = A[i][j] - sum;
-            }
-            sum = 0;
-            for(k = 0; k < j; k++) {
-                sum = sum + L[j][k] * U[k][i];
-            }
-            if (L[j][j] == 0) {				
-                exit(0);
-            }
-            U[j][i] = (A[j][i] - sum) / L[j][j];
-        }
+        #pragma omp parallel for num_threads(number_of_threads) private(i,k,sum)
+        for (int i = j; i < n; i++) {
+			double sum = 0;
+			for(int k = 0; k < j; k++) {
+				sum = sum + L[j][k] * U[k][i];
+			}
+			if (L[j][j] == 0) {				
+				exit(0);
+			}
+			U[j][i] = (A[j][i] - sum) / L[j][j];
+		}
     }
 }	
 
-void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
+void crout2(double **L, double **U, int n, int number_of_threads) {
     int i,j,k;
     double sum = 0, sum1 = 0, sum2 = 0;
-    #pragma omp parallel sections num_threads(number_of_threads)
+    #pragma omp parallel sections num_threads(number_of_threads) private(i)
     {
         #pragma omp section
         {
@@ -243,8 +163,7 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
         #pragma omp parallel sections num_threads(number_of_threads) private(sum,i,k)
         {
             #pragma omp section
-            {
-                // printf("A");
+            {  
                 for (i = j+1 + (0*(n-j-1))/8; i < j+1 + (1*(n-j-1))/8; i++) {
                     sum = 0;
                     for (k = 0; k < j; k++) {
@@ -256,7 +175,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                // printf("B");
                 for (i = j+1 + (1*(n-j-1))/8; i < j+1 + (2*(n-j-1))/8; i++) {
                     sum = 0;
                     for (k = 0; k < j; k++) {
@@ -268,7 +186,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                // printf("C");
                 for (i = j+1 + (2*(n-j-1)/8); i < j+1 + (3*(n-j-1))/8; i++) {
                     sum = 0;
                     for (k = 0; k < j; k++) {
@@ -280,7 +197,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("D");
                 for (i = j+1 + (3*(n-j-1))/8; i < j+1 + (4*(n-j-1))/8; i++) {
                     sum = 0;
                     for (k = 0; k < j; k++) {
@@ -292,7 +208,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("E");
                 for (i = j+1 + (4*(n-j-1))/8; i < j+1 + (5*(n-j-1))/8; i++) {
                     sum = 0;
                     for (k = 0; k < j; k++) {
@@ -304,7 +219,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("F");
                 for (i = j+1 + (5*(n-j-1))/8; i < j+1 + (6*(n-j-1))/8; i++) {
                     sum = 0;
                     for (k = 0; k < j; k++) {
@@ -316,7 +230,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("G");
                 for (i = j+1 + (6*(n-j-1))/8; i < j+1 + (7*(n-j-1))/8; i++) {
                     sum = 0;
                     for (k = 0; k < j; k++) {
@@ -328,7 +241,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("H");
                 for (i = j+1 + (7*(n-j-1))/8; i < j+1 + (8*(n-j-1))/8; i++) {
                     sum = 0;
                     for (k = 0; k < j; k++) {
@@ -340,7 +252,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("I");
                 for (i = j + (0*(n-j))/8; i < j + (1*(n-j))/8; i++) {
                     sum = 0;
                     for(k = 0; k < j; k++) {
@@ -356,7 +267,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("J");
                 for (i = j + (1*(n-j))/8; i < j + (2*(n-j))/8; i++) {
                     sum = 0;
                     for(k = 0; k < j; k++) {
@@ -371,7 +281,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("K");
                 for (i = j + (2*(n-j))/8; i < j + (3*(n-j))/8; i++) {
                     sum = 0;
                     for(k = 0; k < j; k++) {
@@ -386,7 +295,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("L");
                 for (i = j + (3*(n-j))/8; i < j + (4*(n-j))/8; i++) {
                     sum = 0;
                     for(k = 0; k < j; k++) {
@@ -401,7 +309,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("M");
                 for (i = j + (4*(n-j))/8; i < j + (5*(n-j))/8; i++) {
                     sum = 0;
                     for(k = 0; k < j; k++) {
@@ -416,7 +323,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("N");
                 for (i = j + (5*(n-j))/8; i < j + (6*(n-j))/8; i++) {
                     sum = 0;
                     for(k = 0; k < j; k++) {
@@ -431,7 +337,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("O");
                 for (i = j + (6*(n-j))/8; i < j + (7*(n-j))/8; i++) {
                     sum = 0;
                     for(k = 0; k < j; k++) {
@@ -446,7 +351,6 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                //printf("P");
                 for (i = j + (7*(n-j))/8; i < j + (8*(n-j))/8; i++) {
                     sum = 0;
                     for(k = 0; k < j; k++) {
@@ -463,27 +367,25 @@ void crout2(double **A, double **L, double **U, int n, int number_of_threads) {
     }
 }	
 
-void crout3(double **A, double **L, double **U, int n, int number_of_threads) {
-    int i, j, k;
-	double sum = 0;
-    #pragma parallel for num_threads(number_of_threads)
-	for (i = 0; i < n; i++) {
-		U[i][i] = 1;
-	}
-	for (j = 0; j < n; j++) {
-        sum = 0;
-        for (k = 0; k < j; k++) {
+void crout3(double **L, double **U, int n, int number_of_threads) {
+    #pragma omp parallel num_threads(number_of_threads)
+    for (int i = 0; i < n; i++) {
+        U[i][i] = 1;
+    }
+    for (int j = 0; j < n; j++) {
+        double sum = 0;
+        for (int k = 0; k < j; k++) {
             sum = sum + L[j][k] * U[k][j];	
         }
         L[j][j] = A[j][j] - sum;
-        #pragma omp parallel sections private(i,k,sum)
+        #pragma omp parallel sections
         {
             #pragma omp section
             {
-                #pragma parallel for
-                for (i = j; i < n; i++) {
-                    sum = 0;
-                    for (k = 0; k < j; k++) {
+                #pragma omp parallel for num_threads(number_of_threads) 
+                for (int i = j+1; i < n; i++) {
+                    double sum = 0;
+                    for (int k = 0; k < j; k++) {
                         sum = sum + L[i][k] * U[k][j];	
                     }
                     L[i][j] = A[i][j] - sum;
@@ -492,10 +394,10 @@ void crout3(double **A, double **L, double **U, int n, int number_of_threads) {
 
             #pragma omp section
             {
-                #pragma parallel for
-                for (i = j; i < n; i++) {
-                    sum = 0;
-                    for(k = 0; k < j; k++) {
+                #pragma omp parallel for num_threads(number_of_threads)
+                for (int i = j; i < n; i++) {
+                    double sum = 0;
+                    for(int k = 0; k < j; k++) {
                         sum = sum + L[j][k] * U[k][i];
                     }
                     if (L[j][j] == 0) {				
@@ -505,14 +407,14 @@ void crout3(double **A, double **L, double **U, int n, int number_of_threads) {
                 }
             }
         }
-	}
+    }
 }
 
-void print_array(double **A, int n,char *c){ 
+void print_array(double **arr,int n,char *c){ 
     printf("\n--------------------------- %s ---------------------------\n",c);
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++)
-        printf("%f\t",A[i][j]);
+        printf("%f\t",arr[i][j]);
         printf("\n");
     }
     printf("----------------------------------------------------------\n\n");
@@ -531,10 +433,11 @@ void write_output(char fname[], double** arr, int n){
 
 int main(int argc, char* argv[])
 {
+    omp_set_nested(1);
     int n;
     char *input_filename;
     int strategy;
-    double **a;
+    
    
     if (argc != 5) {printf("Enter 5 arguments only. Eg.\"executable_filename n input_filename number_of_threads strategy\"\n");return 0;}
    
@@ -544,9 +447,9 @@ int main(int argc, char* argv[])
     strategy = atoi(argv[4]);
     char *not = argv[3];
    
-    a=malloc(n*sizeof(double*)); 
+    A=(double **)malloc(n*sizeof(double*)); 
     for(int i=0;i<n;++i)
-        a[i]=malloc(n*sizeof(double));
+        A[i]=(double *)malloc(n*sizeof(double));
    
     FILE *fptr;
     if ((fptr = fopen(input_filename, "r")) == NULL) {
@@ -555,35 +458,32 @@ int main(int argc, char* argv[])
     }
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-        if (!fscanf(fptr, "%lf", &a[i][j])) 
+        if (!fscanf(fptr, "%lf", &A[i][j])) 
             break;
         }
     }
     fclose(fptr);
-   //printf("%d %s %d %d\n",n,input_filename,number_of_threads,strategy);
    
-    double **L;
-    double **U;
-   
-    L=malloc(n*sizeof(double*));
-    U=malloc(n*sizeof(double*)); 
-    for(int i=0;i<n;++i) {
-        L[i]=malloc(n*sizeof(double));
-        U[i]=malloc(n*sizeof(double));
-    }
+    L=(double **)malloc(n*sizeof(double*));
+    for(int i=0;i<n;++i) 
+        L[i]=(double *)malloc(n*sizeof(double));
+
+    U=(double **)malloc(n*sizeof(double*)); 
+    for(int i=0;i<n;++i)
+        U[i]=(double *)malloc(n*sizeof(double));
+
     if (strategy==0) {
-        // printf("X");
-        crout0(a,L,U,n);
-        // printf("Y");
-        char *lf = "output_L_0_";
-        char *uf = "output_U_0_";
-        char *extension = ".txt";
+        crout0(L,U,n);
+        char lf[12] = "output_L_0_";
+        char uf[12] = "output_U_0_";
+        char extension[5] = ".txt";
         char *f1 = (char *) malloc(256);
+        f1[0]='\0';
         char *f2 = (char *) malloc(256);
+        f2[0]='\0';
         strcat(f1,lf); strcat(f1,not); strcat(f1,extension);
         strcat(f2,uf); strcat(f2,not); strcat(f2,extension);
 
-        // printf("|%s|%s|\n",f1,f2);
         write_output(f1,L,n);
         write_output(f2,U,n);
         
@@ -591,12 +491,14 @@ int main(int argc, char* argv[])
         // print_array(U,n,"U");
     }
     else if(strategy==1){
-        crout1(a,L,U,n,number_of_threads);
-        char *lf = "output_L_1_";
-        char *uf = "output_U_1_";
-        char *extension = ".txt";
+        crout1(L,U,n,number_of_threads);
+        char lf[12] = "output_L_1_";
+        char uf[12] = "output_U_1_";
+        char extension[5] = ".txt";
         char *f1 = (char *) malloc(256);
+        f1[0]='\0';
         char *f2 = (char *) malloc(256);
+        f2[0]='\0';
         strcat(f1,lf); strcat(f1,not); strcat(f1,extension);
         strcat(f2,uf); strcat(f2,not); strcat(f2,extension);
 
@@ -607,14 +509,17 @@ int main(int argc, char* argv[])
         // print_array(U,n,"U");
     }
     else if(strategy==2){
-        crout2(a,L,U,n,number_of_threads);
-        char *lf = "output_L_2_";
-        char *uf = "output_U_2_";
-        char *extension = ".txt";
+        crout2(L,U,n,number_of_threads);
+        char lf[12] = "output_L_2_";
+        char uf[12] = "output_U_2_";
+        char extension[5] = ".txt";
         char *f1 = (char *) malloc(256);
+        f1[0]='\0';
         char *f2 = (char *) malloc(256);
+        f2[0]='\0';
         strcat(f1,lf); strcat(f1,not); strcat(f1,extension);
         strcat(f2,uf); strcat(f2,not); strcat(f2,extension);
+        
 
         write_output(f1,L,n);
         write_output(f2,U,n);
@@ -623,12 +528,14 @@ int main(int argc, char* argv[])
         // print_array(U,n,"U");
     }
     else if(strategy==3){
-        crout3(a,L,U,n,number_of_threads);
-        char *lf = "output_L_3_";
-        char *uf = "output_U_3_";
-        char *extension = ".txt";
+        crout3(L,U,n,number_of_threads);
+        char lf[12] = "output_L_3_";
+        char uf[12] = "output_U_3_";
+        char extension[5] = ".txt";
         char *f1 = (char *) malloc(256);
+        f1[0]='\0';
         char *f2 = (char *) malloc(256);
+        f2[0]='\0';
         strcat(f1,lf); strcat(f1,not); strcat(f1,extension);
         strcat(f2,uf); strcat(f2,not); strcat(f2,extension);
 
